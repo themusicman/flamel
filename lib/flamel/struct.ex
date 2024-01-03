95454c4de46cc5ea2f63fadeb7b7a6744ffd817e
Thomas Brewer <tom.brewer@mac.com>
2024-01-03 06:10:24 -0500

    feat(structs): adding a fields function for structs

---
+++ lib/flamel/struct.ex
@@ -0,0 +1,22 @@
+defmodule Flamel.Struct do
+  @moduledoc """
+  A bunch of helper functions for Structs
+  """
+
+  @doc """
+  Return a list of fields from a strcut
+
+  ## Examples
+
+      iex> Flamel.Struct.fields(%User{a: 1, b: 2})
+      [:a, :b]
+
+  """
+  def fields(struct) when is_struct(struct) do
+    Map.keys(struct) |> List.delete(:__struct__)
+  end
+
+  def fields(_struct) do
+    []
+  end
+end
---
+++ test/flamel/struct_test.exs
@@ -0,0 +1,21 @@
+defmodule Flamel.MapTest do
+  use ExUnit.Case
+
+  defmodule Person do
+    defstruct name: "", dob: ""
+  end
+
+  alias Person
+
+  describe "fields/1" do
+    test "returns a list of fields from passed a struct" do
+      fields = Flamel.Struct.fields(%Person{})
+      assert fields == [:name, :dob]
+    end
+
+    test "returns an empty list when passed something that is not a struct" do
+      fields = Flamel.Struct.fields(nil)
+      assert fields == []
+    end
+  end
+end
