46f4a8ad50f58a867fdfb0aa5996009afebf771b
Thomas Brewer <tom.brewer@mac.com>
2024-01-06 06:59:16 -0500

    ref(task)!: renaming

--- lib/flamel/task.ex
+++ lib/flamel/task.ex
@@ -1,13 +1,13 @@
-defmodule Backoff.Task do
+defmodule Flamel.Task do
   @doc """
-  Run a task after a specified delay interval.
+  Execute a function after a specified delay interval.
 
   ## Examples
 
-      ie> task = Backoff.Task.delay(1_000, fn -> IO.puts("I am 1 second delayed") end)
+      ie> task = Flamel.Task.delay(1_000, fn -> IO.puts("I am 1 second delayed") end)
       ie> Task.await(task)
   """
-  @spec delay(Backoff.interval(), function()) :: Task.t()
+  @spec delay(timeout(), function()) :: Task.t()
   def delay(interval, func) do
     Task.async(fn ->
       Process.sleep(interval)
